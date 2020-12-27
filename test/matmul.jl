4570ba6a46199ce3036e44629e84ada8773d24e0
Chris Elrod <elrodc@gmail.com>
2020-12-27 03:49:03 -0500

    Test fixes

--- test/matmul.jl
+++ test/matmul.jl
@@ -2,19 +2,22 @@
 @testset "Matrix Multiply" begin
 
     for T ∈ (Float32, Float64, Int32, Int64)
-        for logn ∈ range(log(1), log(4000), length = 50)
+        for logn ∈ range(log(1), log(4000), length = 5)
             n = round(Int, exp(logn))
-            for logk ∈ range(log(1), log(4000), length = 50)
+            for logk ∈ range(log(1), log(4000), length = 5)
                 k = round(Int, exp(logk))
                 B = rand(T, k, n)
-                for logm ∈ range(log(1), log(4000), length = 50)
+                B′ = permutedims(B)'
+                for logm ∈ range(log(1), log(4000), length = 5)
                     m = round(Int, exp(logm))
                     A = rand(T, m, k)
+                    A′ = permutedims(A)'
 
-                    @test matmul(A, B) ≈ A * B
-                    @test matmul(A', B) ≈ A' * B
-                    @test matmul(A, B') ≈ A * B'
-                    @test matmul(A', B') ≈ A' * B'
+                    @show m, k, n
+                    @test @time(Octavian.matmul(A, B)) ≈ A * B
+                    @test @time(Octavian.matmul(A′, B)) ≈ A′ * B
+                    @test @time(Octavian.matmul(A, B′)) ≈ A * B′
+                    @test @time(Octavian.matmul(A′, B′)) ≈ A′ * B′
                 end
             end
         end
