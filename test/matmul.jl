9445ac58b032152cc9dd8573c0d38b8506b8ef81
Dilum Aluthge <dilum@aluthge.com>
2020-12-27 17:35:29 -0500

    Reduce size range of matrices being tested

--- test/matmul.jl
+++ test/matmul.jl
@@ -2,13 +2,13 @@
 @testset "Matrix Multiply" begin
 
     for T ∈ (Float32, Float64, Int32, Int64)
-        for logn ∈ range(log(1), log(4000), length = 5)
+        for logn ∈ range(log(1), log(400), length = 5)
             n = round(Int, exp(logn))
-            for logk ∈ range(log(1), log(4000), length = 5)
+            for logk ∈ range(log(1), log(400), length = 5)
                 k = round(Int, exp(logk))
                 B = rand(T, k, n)
                 B′ = permutedims(B)'
-                for logm ∈ range(log(1), log(4000), length = 5)
+                for logm ∈ range(log(1), log(400), length = 5)
                     m = round(Int, exp(logm))
                     A = rand(T, m, k)
                     A′ = permutedims(A)'
