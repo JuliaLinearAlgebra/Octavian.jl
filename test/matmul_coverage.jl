60689ddf2181a2125dd5935f416f1be3a9393230
chriselrod <elrodc@gmail.com>
2021-01-04 08:00:27 -0500

    Added explicit dependency on ArrayInterface (#29)

--- test/matmul_coverage.jl
+++ test/matmul_coverage.jl
@@ -57,3 +57,19 @@ end
     @test @time(Octavian.matmul(A, B′)) == A * B′
     @test @time(Octavian.matmul(A′, B′)) == A′ * B′
 end
+
+@time @testset "A not-a-StrideArray" begin
+    m = 20
+    n = 20
+    k = 20
+    A = view(rand(Float64, 2m, 2k), 1:2:2m, 2:2:2k)
+    B = rand(Float64, k, n)
+    A′ = view(permutedims(parent(A))', 1:2:2m, 2:2:2k)
+    B′ = permutedims(B)'
+    @show m, k, n
+    @test @time(Octavian.matmul(A, B)) ≈ A * B
+    @test @time(Octavian.matmul(A′, B)) ≈ A′ * B
+    @test @time(Octavian.matmul(A, B′)) ≈ A * B′
+    @test @time(Octavian.matmul(A′, B′)) ≈ A′ * B′
+end
+
