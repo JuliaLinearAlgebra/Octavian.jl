fc8fe0d846f170a49d2295d9a91387f66f117878
Dilum Aluthge <dilum@aluthge.com>
2021-01-20 09:48:51 -0500

    Coverage: test with really big matrices

--- test/matmul_coverage.jl
+++ test/matmul_coverage.jl
@@ -1,6 +1,10 @@
-n_values  = [10, 20, 50, 100, 150, 200]
-k_values  = [10, 20, 50, 100, 150, 200]
-m_values  = [10, 20, 50, 100, 150, 200]
+n_values  = [10, 20, 50]
+k_values  = [10, 20, 50]
+m_values  = [10, 20, 50]
+
+additional_n_k_m_values = [
+    (2000, 2000, 2000),
+]
 
 testset_name_suffix = "(coverage)"
 
--- test/matmul_main.jl
+++ test/matmul_main.jl
@@ -2,6 +2,8 @@ n_values  = [200, 300, 400]
 k_values  = [200, 300, 400]
 m_values  = [200, 300, 400]
 
+additional_n_k_m_values = []
+
 testset_name_suffix = "(main)"
 
 include("_matmul.jl")
