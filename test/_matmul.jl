daa0677f49c37a84b10bcf1c1512e322b731c26b
MasonProtter <mason.protter@icloud.com>
2021-05-17 17:16:43 -0600

    restore accidentally deleted F32 test

--- test/_matmul.jl
+++ test/_matmul.jl
@@ -87,6 +87,40 @@ end
     @test matmul_pack_ab!(similar(AB), A′, B′) ≈ AB
 end
 
+@time @testset "Matrix Multiply Float32 $(testset_name_suffix)" begin
+    T = Float32
+    for n ∈ n_values
+        for k ∈ k_values
+            for m ∈ m_values
+                A = rand(T, m, k)
+                B = rand(T, k, n)
+                A′ = permutedims(A)'
+                B′ = permutedims(B)'
+                AB = A * B;
+                @info "" T n k m
+                @test @time(Octavian.matmul(A, B)) ≈ AB
+                @test @time(Octavian.matmul(A′, B)) ≈ AB
+                @test @time(Octavian.matmul(A, B′)) ≈ AB
+                @test @time(Octavian.matmul(A′, B′)) ≈ AB
+                @test @time(Octavian.matmul_serial(A, B)) ≈ AB
+                @test @time(Octavian.matmul_serial(A′, B)) ≈ AB
+                @test @time(Octavian.matmul_serial(A, B′)) ≈ AB
+                @test @time(Octavian.matmul_serial(A′, B′)) ≈ AB
+            end
+        end
+    end
+    m = k = n = max(8Octavian.OCTAVIAN_NUM_TASKS[], 400)
+    A = rand(T, m, k);
+    B = rand(T, k, n);
+    A′ = permutedims(A)';
+    B′ = permutedims(B)';
+    AB = A * B;
+    @test matmul_pack_ab!(similar(AB), A, B) ≈ AB
+    @test matmul_pack_ab!(similar(AB), A, B′) ≈ AB
+    @test matmul_pack_ab!(similar(AB), A′, B) ≈ AB
+    @test matmul_pack_ab!(similar(AB), A′, B′) ≈ AB
+end
+
 @time @testset "Matrix Multiply Int32 $(testset_name_suffix)" begin
     T = Int32
     for n ∈ n_values
