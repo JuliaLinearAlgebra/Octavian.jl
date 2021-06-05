521065b7d1234c8e2a530f560f1557551b4a75d8
Chris Elrod <elrodc@gmail.com>
2021-06-05 14:18:10 -0400

    Fix documentation.

--- src/matmul.jl
+++ src/matmul.jl
@@ -220,7 +220,7 @@ end
 """
     matmul!(C, A, B[, α, β, max_threads])
 
-Calculates `C = α * A * B + β * C` in place, overwriting the contents of `A`.
+Calculates `C = α * A * B + β * C` in place, overwriting the contents of `C`.
 It may use up to `max_threads` threads. It will not use threads when nested in other threaded code.
 """
 @inline function matmul!(C::AbstractVecOrMat, A::AbstractMatrix, B::AbstractVecOrMat)
