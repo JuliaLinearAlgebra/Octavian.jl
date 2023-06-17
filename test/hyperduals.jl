ae5c086658267e9ad31e8c3fd54d35d22fdc5b4d
cheukhinhojerry <cheukhinhojerry@gmail.com>
2023-06-16 20:25:47 -0700

    tests fix with proper testsets

--- test/hyperduals.jl
+++ test/hyperduals.jl
@@ -20,36 +20,69 @@ end
   B1 = rand(Float64, k, n)
   C1 = rand(Float64, m, n)
 
-  A2 = deepcopy(A1)
-  B2 = deepcopy(B1)
-  C2 = deepcopy(C1)
-
   α = Float64(2.0)
   β = Float64(2.0)
 
-  Octavian.matmul!(C1, A1, B1, α, β)
-  LinearAlgebra.mul!(C2, A2, B2, α, β)
-  @test C1 ≈ C2
+  @testset "real array from the right" begin
+    A1dual = zeros(HyperDualNumbers.Hyper{Float64}, size(A1)...)
+    C1dual = zeros(HyperDualNumbers.Hyper{Float64}, size(C1)...)
 
-  A1dual = zeros(HyperDualNumbers.Hyper{Float64}, reverse(size(A1))...)
-  A1dual .= A1'
-  C1dual = zeros(HyperDualNumbers.Hyper{Float64}, size(C1)...)
+    A1dual .= A1
+    C1dual .= C1
+    
+    A2dual = deepcopy(A1dual)
+    B2 = deepcopy(B1)
+    C2dual = deepcopy(C1dual)
 
-  A2dual = deepcopy(A1dual)
-  C2dual = deepcopy(C1dual)
-  C3dual = similar(C1dual)
-  C4dual = similar(C2dual)
-  Octavian.matmul!(C1dual, A1dual', B1)
-  Octavian.matmul!(C2dual, A2dual', B2)
-  Octavian.matmul_serial!(C3dual, A1dual', B1)
-  Octavian.matmul_serial!(C4dual, A2dual', B2)
+    Octavian.matmul!(C1dual, A1dual, B1, α, β)
+    LinearAlgebra.mul!(C2dual, A2dual, B2, α, β)
+    @test reinterpretHD(Float64, C1dual) ≈ reinterpretHD(Float64, C2dual)
+  end
 
-  Cref = zeros(Float64, size(C1)...)
-  LinearAlgebra.mul!(Cref, A1, B1)
-  @test reinterpretHD(Float64, C1dual) ≈ reinterpretHD(Float64, C2dual) ≈ reinterpretHD(Float64, C3dual) ≈ reinterpretHD(Float64, C4dual) ≈ Cref
+  @testset "real array from the left" begin
+    B1dual = zeros(HyperDualNumbers.Hyper{Float64}, size(B1)...)
+    C1dual = zeros(HyperDualNumbers.Hyper{Float64}, size(C1)...)
 
+    B1dual .= B1
+    C1dual .= C1
 
-  @testset "two dual arrays" begin
+    A2 = deepcopy(A1)
+    B2dual = deepcopy(B1dual)
+    C2dual = deepcopy(C1dual)
+
+    α = Float64(2.0)
+    β = Float64(2.0)
+
+    Octavian.matmul!(C1dual, A1, B1dual, α, β)
+    LinearAlgebra.mul!(C2dual, A2, B2dual, α, β)
+    @test reinterpretHD(Float64, C1dual) ≈ reinterpretHD(Float64, C2dual)
+  end
+
+  
+  @testset "transposed arrays" begin
+
+    A1dual = zeros(HyperDualNumbers.Hyper{Float64}, reverse(size(A1))...)
+    A1dual .= A1'
+    C1dual = zeros(HyperDualNumbers.Hyper{Float64}, size(C1)...)
+
+    
+    A2dual = deepcopy(A1dual)
+    B2 = deepcopy(B1)
+    C2dual = deepcopy(C1dual)
+
+    C3dual = similar(C1dual)
+    C4dual = similar(C2dual)
+    Octavian.matmul!(C1dual, A1dual', B1)
+    Octavian.matmul!(C2dual, A2dual', B2)
+    Octavian.matmul_serial!(C3dual, A1dual', B1)
+    Octavian.matmul_serial!(C4dual, A2dual', B2)
+
+    Cref = zeros(Float64, size(C1)...)
+    LinearAlgebra.mul!(Cref, A1, B1)
+    @test reinterpretHD(Float64, C1dual) ≈ reinterpretHD(Float64, C2dual) ≈ reinterpretHD(Float64, C3dual) ≈ reinterpretHD(Float64, C4dual) ≈ Cref
+  end
+
+  @testset "two dual arrays" begin 
     A1d = randdual.(A1)
     B1d = randdual.(B1)
     @test reinterpretHD(Float64, Octavian.matmul(A1d, B1d, 1.3)) ≈
